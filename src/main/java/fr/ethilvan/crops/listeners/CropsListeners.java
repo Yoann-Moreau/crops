07da4c34b8df9abc4d01fcd947025fe776114d41
Yoann Moreau <yoann.moreau@tutanota.com>
2023-12-29 19:25:59 +0100

    Prevent destruction of crops when using a hoe and reset growth if fully grown

--- src/main/java/fr/ethilvan/crops/listeners/CropsListeners.java
+++ src/main/java/fr/ethilvan/crops/listeners/CropsListeners.java
@@ -2,9 +2,16 @@ package fr.ethilvan.crops.listeners;
 
 import fr.ethilvan.crops.Crops;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.Ageable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
 
 public class CropsListeners implements Listener {
 
@@ -16,16 +23,45 @@ public class CropsListeners implements Listener {
 
 	@EventHandler
 	public void onBlockBreak(BlockBreakEvent e) {
-		Material item = e.getPlayer().getInventory().getItemInMainHand().getType();
-		boolean hoe = item.equals(Material.WOODEN_HOE) ||
-				item.equals(Material.STONE_HOE) ||
-				item.equals(Material.IRON_HOE) ||
-				item.equals(Material.GOLDEN_HOE) ||
-				item.equals(Material.DIAMOND_HOE) ||
-				item.equals(Material.NETHERITE_HOE);
+		ItemStack item = e.getPlayer().getInventory().getItemInMainHand();
+		boolean hoe = item.getType().equals(Material.WOODEN_HOE) ||
+				item.getType().equals(Material.STONE_HOE) ||
+				item.getType().equals(Material.IRON_HOE) ||
+				item.getType().equals(Material.GOLDEN_HOE) ||
+				item.getType().equals(Material.DIAMOND_HOE) ||
+				item.getType().equals(Material.NETHERITE_HOE);
 
-		if (e.getBlock().getType().equals(Material.WHEAT) && hoe) {
+		Block block = e.getBlock();
+
+		if (hoe &&
+				(block.getType().equals(Material.WHEAT) ||
+						block.getType().equals(Material.CARROTS) ||
+						block.getType().equals(Material.POTATOES) ||
+						block.getType().equals(Material.BEETROOTS)
+				)
+		) {
+			Ageable ageable = (Ageable) e.getBlock().getBlockData();
+
 			e.setCancelled(true);
+
+			if (ageable.getAge() == ageable.getMaximumAge()) {
+
+				@NotNull Collection<ItemStack> drops = block.getDrops();
+				drops.forEach(drop -> {
+					if (!drop.getType().equals(Material.WHEAT_SEEDS) &&
+							!drop.getType().equals(Material.BEETROOT_SEEDS)) {
+						block.getWorld().dropItem(block.getLocation(), drop);
+					}
+				});
+
+				ageable.setAge(1);
+				block.setBlockData(ageable);
+
+				Damageable damageable = (Damageable) item.getItemMeta();
+				int damage = damageable.getDamage();
+				damageable.setDamage(damage + 1);
+				item.setItemMeta(damageable);
+			}
 		}
 	}
 }
