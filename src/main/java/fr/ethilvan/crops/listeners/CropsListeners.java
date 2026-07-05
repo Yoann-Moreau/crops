28b9ac1e6d2ebc4836f5a87861f4cceee840626f
Yoann Moreau <yoann.moreau@tutanota.com>
2026-07-05 18:31:26 +0200

    Upgrade Paper version to 26.1.2 & small refactoring

--- src/main/java/fr/ethilvan/crops/listeners/CropsListeners.java
+++ src/main/java/fr/ethilvan/crops/listeners/CropsListeners.java
@@ -12,64 +12,84 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.Damageable;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
 import java.util.Collection;
+
 
 public class CropsListeners implements Listener {
 
-	public Crops crops;
+	public Crops plugin;
 
-	public CropsListeners(Crops crops) {
-		this.crops = crops;
+	private final Material[] tools = new Material[]{
+			Material.WOODEN_HOE,
+			Material.COPPER_HOE,
+			Material.IRON_HOE,
+			Material.GOLDEN_HOE,
+			Material.DIAMOND_HOE,
+			Material.NETHERITE_HOE,
+	};
+
+	private final Material[] crops = new Material[]{
+			Material.WHEAT,
+			Material.CARROTS,
+			Material.POTATOES,
+			Material.BEETROOTS,
+	};
+
+	private final Material[] dropsToReduce = new Material[]{
+			Material.WHEAT_SEEDS,
+			Material.BEETROOT_SEEDS,
+			Material.CARROT,
+			Material.POTATO,
+	};
+
+
+	public CropsListeners(Crops plugin) {
+		this.plugin = plugin;
 	}
+
 
 	@EventHandler
 	public void onBlockBreak(BlockBreakEvent e) {
+		// Item in hand
 		ItemStack item = e.getPlayer().getInventory().getItemInMainHand();
-		boolean hoe = item.getType().equals(Material.WOODEN_HOE) ||
-				item.getType().equals(Material.STONE_HOE) ||
-				item.getType().equals(Material.IRON_HOE) ||
-				item.getType().equals(Material.GOLDEN_HOE) ||
-				item.getType().equals(Material.DIAMOND_HOE) ||
-				item.getType().equals(Material.NETHERITE_HOE);
+		boolean isHoe = Arrays.asList(tools).contains(item.getType());
 
+		// Block hit
 		Block block = e.getBlock();
 
-		if (hoe &&
-				(block.getType().equals(Material.WHEAT) ||
-						block.getType().equals(Material.CARROTS) ||
-						block.getType().equals(Material.POTATOES) ||
-						block.getType().equals(Material.BEETROOTS)
-				)
-		) {
+		if (isHoe && Arrays.asList(crops).contains(block.getType())) {
 			Ageable ageable = (Ageable) e.getBlock().getBlockData();
 
 			e.setCancelled(true);
 
-			if (ageable.getAge() == ageable.getMaximumAge()) {
+			// Skip if not completely grown
+			if (ageable.getAge() != ageable.getMaximumAge()) {
+				return;
+			}
 
-				@NotNull Collection<ItemStack> drops = block.getDrops();
-				for (ItemStack drop : drops) {
-					if (drop.getType().equals(Material.WHEAT_SEEDS) || drop.getType().equals(Material.BEETROOT_SEEDS)) {
-						continue;
-					}
-					if (drop.getType().equals(Material.CARROT) || drop.getType().equals(Material.POTATO)) {
-						drop.setAmount(drop.getAmount() - 1);
-					}
-					block.getWorld().dropItem(block.getLocation(), drop);
+			// Reduce drops if item used as seed
+			@NotNull Collection<ItemStack> drops = block.getDrops();
+			for (ItemStack drop : drops) {
+				if (Arrays.asList(dropsToReduce).contains(drop.getType())) {
+					drop.setAmount(drop.getAmount() - 1);
 				}
+				block.getWorld().dropItem(block.getLocation(), drop);
+			}
 
-				ageable.setAge(0);
-				block.setBlockData(ageable);
+			// Reset age of crop (first stage)
+			ageable.setAge(0);
+			block.setBlockData(ageable);
 
-				Damageable damageable = (Damageable) item.getItemMeta();
-				damageable.setDamage(damageable.getDamage() + 1);
-				if (damageable.getDamage() >= item.getType().getMaxDurability()) {
-					item.setAmount(0);
-					e.getPlayer().getWorld().playSound(e.getPlayer().getLocation(), Sound.ENTITY_ITEM_BREAK, 1.0f, 1.0f);
-				}
-				else {
-					item.setItemMeta(damageable);
-				}
+			// Damage tool
+			Damageable damageable = (Damageable) item.getItemMeta();
+			damageable.setDamage(damageable.getDamage() + 1);
+			if (damageable.getDamage() >= item.getType().getMaxDurability()) {
+				item.setAmount(0);
+				e.getPlayer().getWorld().playSound(e.getPlayer().getLocation(), Sound.ENTITY_ITEM_BREAK, 1.0f, 1.0f);
+			}
+			else {
+				item.setItemMeta(damageable);
 			}
 		}
 	}
